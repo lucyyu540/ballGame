d3a1494a9583f88f0877fae67dc0afaeb790700f
Lucy Yu <lucyyu540@gmail.com>
2019-11-23 13:10:15 -0500

    mouse control to key control

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ let paddleA, paddleB, ball, wallTop, wallBottom;
 
 
 let MAX_SPEED = 5;
+let paddleSpeed=5;
 
 
 function setup() {
@@ -37,14 +38,60 @@ function draw() {
 
   background(255, 255, 255);
 
-  paddleA.position.y = constrain(mouseY, paddleA.height / 2, height - paddleA.height / 2);
-  paddleB.position.y = constrain(mouseY, paddleA.height / 2, height - paddleA.height / 2);
+  //paddleA.position.y = constrain(mouseY, paddleA.height / 2, height - paddleA.height / 2);
+  //paddleB.position.y = constrain(mouseY, paddleA.height / 2, height - paddleA.height / 2);
   ball.bounce(wallTop);
   ball.bounce(wallBottom);
+    if (keyDown('a')) {
+    paddleA.position.y = paddleA.position.y + paddleSpeed;
+  }
 
-  ball.bounce(paddleA);
-  ball.bounce(paddleB);
-  //let swing;
+  if (keyDown('s')) {
+    paddleA.position.y = paddleA.position.y - paddleSpeed;
+  }
+  
+    if (keyDown('o')) {
+    paddleB.position.y = paddleB.position.y + paddleSpeed;
+  }
+
+  if (keyDown('p')) {
+    paddleB.position.y = paddleB.position.y - paddleSpeed;
+  }
+
+	//ball.bounce(paddleA);
+  //ball.bounce(paddleB);
+  //make the bounce more interesting
+  let swing;
+
+  if (ball.bounce(paddleA)) {
+
+    swing = (ball.position.y - paddleA.position.y) / 3;
+    ball.setSpeed(MAX_SPEED, ball.getDirection() + swing);
+
+  }
+  
+    if (ball.bounce(paddleB)) {
+
+    swing = (ball.position.y - paddleB.position.y) / 3;
+    ball.setSpeed(MAX_SPEED, ball.getDirection() - swing);
+
+  }
+
+  if (ball.position.x < 0) {
+    ball.position.x = width / 2;
+    ball.position.y = height / 2;
+    ball.setSpeed(MAX_SPEED, 0);
+  }
+
+  if (ball.position.x > width) {
+
+    ball.position.x = width / 2;
+    ball.position.y = height / 2;
+    ball.setSpeed(MAX_SPEED, 180);
+  }
+
+
+
 
   drawSprites();
   
